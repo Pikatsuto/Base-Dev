7da6154f1dbf479f5977078aee6d761e27a868ae
sigmanificient <edhyjox@gmail.com>
2022-06-19 19:08:24 +0200

    :construction: Added wip action handler

--- basedev/__main__.py
+++ basedev/__main__.py
@@ -1,177 +1,38 @@
+from termcolor import colored
+
+from .command import CommandGroup
 from libs.package import help, console, error_and_log
 
 fileName = "Main"
 
 
 def main():
-    function_name = "console"
+    is_running = True
 
-    try:
-        commands = {
-            "allCommand": {
-                "clear": {"command": console.clear, "arguments": False},
-                "?": {
-                    "command": help.start_init,
-                    "arguments": {"default": [False], "dev": True},
-                },
-                "debug": {
-                    "command": error_and_log.debug_mode,
-                    "arguments": {"default": [True], "o": True, "n": False},
-                },
-            }
-        }
+    console.clear()
+    print(
+        colored(f"# {'-' * 75} #", "green"),
+        '\n'
+        '   888888b.                                     8888888b.\n'
+        '   888  "88b                                    888  "Y88b\n'
+        '   888  .88P                                    888    888\n'
+        '   8888888K.   8888b.  .d8888b   .d88b.         888    888  .d88b.  888  888\n'
+        '   888  "Y88b     "88b 88K      d8P  Y8b        888    888 d8P  Y8b 888  888\n'
+        '   888    888 .d888888 "Y8888b. 88888888        888    888 88888888 Y88  88P\n'
+        '   888   d88P 888  888      X88 Y8b.            888  .d88P Y8b.      Y8bd8P\n'
+        '   8888888P"  "Y888888  88888P\'  "Y8888         8888888P"   "Y8888    Y88P\n'
+        '\n'
+        '                                  By Pikatsuto\n'
+        + colored(f"# {'-' * 75} #", "green"),
+        '\n\n'
+        + colored("Welcome", "green"), "for Base Dev shell\n"
+        + colored("[?]", "cyan"), "For Help command\n",
+    )
 
-        start = False
-        while True:
-            error_and_log.debug_test(
-                function=function_name, my_file=fileName, number=0, condition="Initial"
-            )
-            console_input = console.console_input_handler()
-
-            if start:
-                error_and_log.debug_test(
-                    function=function_name,
-                    my_file=fileName,
-                    number=1,
-                    condition="if first start",
-                )
-                start = False
-                console.clear()
-
-            elif console_input == "":
-                error_and_log.debug_test(
-                    function=function_name,
-                    my_file=fileName,
-                    number=1,
-                    condition='elif consoleInput == ""',
-                )
-            else:  # check if console input is on command json
-                error_and_log.debug_test(
-                    function=function_name,
-                    my_file=fileName,
-                    number=1,
-                    condition="else: check json command",
-                )
-                console_input_split = console_input.split(" ")
-                arguments = console_input_split
-                command_save = False
-                arguments_ok = False
-
-                command = commands["allCommand"]
-                if console_input_split[0] in command:
-                    error_and_log.debug_test(
-                        function=function_name,
-                        my_file=fileName,
-                        number=2,
-                        condition="if your command in all command",
-                    )
-                    command_save = command[console_input_split[0]]["command"]
-                    if command[console_input_split[0]]["arguments"]:
-                        error_and_log.debug_test(
-                            function=function_name,
-                            my_file=fileName,
-                            number=3,
-                            condition="if your command have argument",
-                        )
-                        if (
-                            not command[console_input_split[0]]["arguments"]["default"]
-                            and len(arguments) == 1
-                        ):
-                            error_and_log.debug_test(
-                                function=function_name,
-                                my_file=fileName,
-                                number=4,
-                                condition="if your command have default argument",
-                            )
-                            arguments = command[console_input_split[0]]["arguments"][
-                                "default"
-                            ][0]
-                            arguments_ok = True
-                        else:
-                            error_and_log.debug_test(
-                                function=function_name,
-                                my_file=fileName,
-                                number=4,
-                                condition="else your command not have default argument",
-                            )
-                            for argumentsInput in arguments:
-                                error_and_log.debug_test(
-                                    function=function_name,
-                                    my_file=fileName,
-                                    number=5,
-                                    condition="for your argument on all argument",
-                                )
-                                if (
-                                    argumentsInput
-                                    in command[console_input_split[0]]["arguments"]
-                                ):
-                                    error_and_log.debug_test(
-                                        function=function_name,
-                                        my_file=fileName,
-                                        number=6,
-                                        condition="if your argument in all argument",
-                                    )
-                                    arguments = command[console_input_split[0]][
-                                        "arguments"
-                                    ][argumentsInput]
-                                    arguments_ok = True
-                                    break
-                                else:
-                                    arguments_ok = False
-                            if not arguments_ok:
-                                error_and_log.debug_test(
-                                    function=function_name,
-                                    my_file=fileName,
-                                    number=5,
-                                    condition="argument not fund set default argument",
-                                )
-                                arguments = command[console_input_split[0]][
-                                    "arguments"
-                                ]["default"][0]
-                                arguments_ok = True
-
-            if command_save and arguments_ok:  # Execute function for command
-                error_and_log.debug_test(
-                    function=function_name,
-                    my_file=fileName,
-                    number=1,
-                    condition="if your command have argument",
-                )
-                command_save(arguments)
-
-            elif command_save:
-                error_and_log.debug_test(
-                    function=function_name,
-                    my_file=fileName,
-                    number=1,
-                    condition="elif your command not have argument",
-                )
-                command_save()
-            else:
-                error_and_log.log_handler(
-                    f'command "{console_input}" not found', True, "log"
-                )
-
-    except Exception as e:
-        return error_and_log.error_handler(e, f"{fileName} {function_name}")
-
-
-def start_init():
-    function_name = "startInit"
-
-    try:
-        error_and_log.debug_test(
-            function=function_name,
-            my_file=fileName,
-            number=1,
-            condition="Start Program",
-        )
-        console.start_init()
-        main()
-
-    except Exception as e:
-        return error_and_log.error_handler(e, f"{fileName} {function_name}")
+    while is_running:
+        action = input(f'{colored("green")}>> ')
+        CommandGroup.methods.get(action)[0]()
 
 
 if __name__ == '__main__':
-    start_init()
+    main()
